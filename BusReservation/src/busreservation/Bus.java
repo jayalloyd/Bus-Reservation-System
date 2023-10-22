46024eb4d0413521939b15742a2988723833a5b2
jayarani83 <jayalloyd83@gmail.com>
2023-10-22 20:30:12 +0530

    updated

--- BusReservation/src/busreservation/Bus.java
+++ BusReservation/src/busreservation/Bus.java
@@ -2,13 +2,48 @@ package busreservation;
 
 
 public class Bus {
-	int busno;
-	boolean ac;
-	int capacity;
+	private int busno;
+	private boolean ac;
+	private int capacity;
 	
+	Bus(int no,boolean ac,int cap){
+		this.setBusno(no);
+		this.setAc(ac);
+		this.setCapacity(cap);
+				
+		
+	}
 
 	public static void main(String[] args) {
 		
+	}
+
+	public int getBusno() {
+		return busno;
+	}
+
+	public void setBusno(int busno) {
+		this.busno = busno;
+	}
+
+	public boolean isAc() {
+		return ac;
+	}
+
+	public void setAc(boolean ac) {
+		this.ac = ac;
+	}
+
+	public int getCapacity() {
+		return capacity;
+	}
+
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+	
+	public void displayBusInfo() {
+		System.out.println("Bus number :"+busno+"AC" +ac +"Total Capacity"+capacity);
 	}
 
 }
--- BusReservation/src/busreservation/BusDemo.java
+++ BusReservation/src/busreservation/BusDemo.java
@@ -1,10 +1,19 @@
 package busreservation;
 import java.util.*;
+import java.util.ArrayList;
 public class BusDemo {
 
 	public static void main(String[] args) {
+		ArrayList<Bus> buses=new ArrayList<Bus>();
+		buses.add(new Bus(1, true ,20));
+		buses.add(new Bus(2,false,40));
+		buses.add(new Bus(5,true,50));
+		
 		int useroption=1;
 	Scanner scanner = new Scanner(System.in);
+	for(Bus b:buses) {
+		b.displayBusInfo();
+	}
 		while(useroption==1)
 		{
 			System.out.println("Enter a value= 1 for new booking and 2 to exit");
@@ -12,5 +21,6 @@ public class BusDemo {
 			if(useroption==1) {
 				System.out.println("Booking Started.....");
 			}
-		}
+			else
+			System.out.println("Booking Stopped...");	}
 	}}
